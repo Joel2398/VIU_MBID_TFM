1f6f79149cab4d2fda06de8a112df1500ea5533e
Joel2398 <joel.rojas2398@gmail.com>
2024-09-08 00:08:53 +0200

    Hay que volver atrás...

--- app_without_edge/fuentes/mqtt_client/app.py
+++ app_without_edge/fuentes/mqtt_client/app.py
@@ -1,22 +1,22 @@
-import os 
+import os
 import json
 import paho.mqtt.client as mqtt
 from influxdb import InfluxDBClient
 import logging
 import time
-import re 
+import re
 
 logging.getLogger().setLevel(logging.INFO)
 
+METADATA_FILE = '/tmp/metadata.json'
 
 def readFile(filename):
-    with open('/cfg-data/'+filename) as f:
+    with open('/cfg-data/' + filename) as f:
         data = json.load(f)
-        logging.info('File succesfully read')
+        logging.info('File successfully read')
         return data
-   
 
-# Function that is reading json file 
+# Function that is reading json file
 data = readFile('env-config.json')
 logging.info("Los datos son {}".format(data))
 MQTT_BROKER_SERVER = data['env']['MQTT_BROKER_SERVER']
@@ -27,17 +27,26 @@ MQTT_TOPIC_DATA = data['env']['MQTT_TOPIC_DATA']
 INFLUXDB_IP = data['env']['INFLUXDB_IP']
 INFLUXDB_DATABASE = data['env']['INFLUXDB_DATABASE']
 
+metadata_payload = None
+
 def save_metadata(payload):
-    ###We need to keep metadata payload to be able to extract tag names comparing the ids we receive wiht the data payload
-    # Para entender esto ver Doc OPC-UA Connector IE en el capitulo 8.4.1 y 8.4.2
     global metadata_payload
-    for key,value in payload.items():
-        if (key == 'seq'):
-            if (value):
-                metadata_payload = payload
-                logging.info('Metadata is:' + str(metadata_payload))
-                exit
-        
+    for key, value in payload.items():
+        if key == 'seq' and value:
+            metadata_payload = payload
+            logging.info('Metadata is:' + str(metadata_payload))
+            with open(METADATA_FILE, 'w') as f:
+                json.dump(metadata_payload, f)
+            return
+
+def load_metadata():
+    global metadata_payload
+    if os.path.exists(METADATA_FILE):
+        with open(METADATA_FILE, 'r') as f:
+            metadata_payload = json.load(f)
+            logging.info('Loaded metadata from file: ' + str(metadata_payload))
+    else:
+        logging.warning('Metadata file does not exist.')
 
 def extract_connection_name(topic):
     try:
@@ -45,97 +54,55 @@ def extract_connection_name(topic):
         logging.info('Connection name is:' + str(connection_name))
         return connection_name
     except AttributeError:
-        pass
+        return None
 
 def extract_names_from_metadata(tag_id, connection_name):
-    # Inicializa la variable tag_name al principio
-    tag_name = None
+    if not metadata_payload:
+        logging.warning('Metadata payload is not available.')
+        return None
 
-    # Función para extraer el nombre del tag a partir de su ID en los metadatos
+    tag_name = None
     for key, value in metadata_payload.items():
         if key == 'connections':
-            elements = value  # Almacena las conexiones
-            break  # Usa break en lugar de exit para salir del bucle
+            elements = value
+            break
+    else:
+        return None
 
     for element in elements:
         for key, value in element.items():
             if key == 'name':
                 if value == connection_name:
-                    continue  # Sigue al siguiente bloque si hay coincidencia
+                    continue
                 else:
-                    break  # Sal del bucle si no hay coincidencia
-
+                    break
             if key == 'dataPoints':
-                dataPoints = value  # Almacena los puntos de datos
-                break  # Usa break para salir del bucle
+                dataPoints = value
+                break
+        else:
+            continue
+        break
 
     for datapoint in dataPoints:
         for key, value in datapoint.items():
             if key == 'dataPointDefinitions':
-                definitions = value  # Almacena las definiciones de puntos de datos
-                break  # Usa break para salir del bucle
+                definitions = value
+                break
+        else:
+            continue
+        break
 
     for definition in definitions:
-        # Reorganiza las condiciones para asignar tag_name correctamente
-        tag_name_found = False  # Bandera para indicar si se encuentra el nombre del tag
+        tag_name_found = False
         tag_name = None
-
         for key, value in definition.items():
             if key == 'id' and value == tag_id:
-                tag_name_found = True  # Marca como encontrado el ID
+                tag_name_found = True
             elif key == 'name' and tag_name_found:
-                tag_name = value  # Asigna el nombre del tag
+                tag_name = value
                 return tag_name
 
-    return tag_name  # Devuelve tag_name, aunque sea None
-
-'''def extract_names_from_metadata(tag_id, connection_name):
-    # Inicializo tag_name por si key == Name pero id no
-    tag_name = None
-
-    ###Function to extract the from every tag wiht an ID given from Databus Metadata
-    for key,value in metadata_payload.items():
-        if (key == 'connections'):
-                elements = value
-                exit
-    for element in elements:
-        for key,value in element.items():
-            if (key == 'name'):
-                if (value == connection_name):
-                    continue
-                else:
-                    break
-            if (key == 'dataPoints'):
-                dataPoints = value
-                exit
-    
-    for datapoint in dataPoints:
-        for key,value in datapoint.items():
-            if (key == 'dataPointDefinitions'):
-                definitions = value
-                exit
-    
-    for definition in definitions:
-        for key,value in definition.items():
-            if (key == 'name'):
-                tag_name = value
-            if (key == 'id'):
-                if (value == tag_id):
-                    return tag_name'''
-
-'''
-def build_json_influx(dict_values, connection_name):
-    logging.info('Data are sending to database is:'+ str(dict_values))
-    json_fields = (dict_values)
-    json_body = [
-        {
-            "measurement" : connection_name,
-            "fields": json_fields
-        }
-    ]
-    logging.info('Data are sending to database is:'+ str(type(json_body)))
-    influx_client.write_points(json_body)
-'''
+    return tag_name
 
 def build_json_influx(dict_values, connection_name):
     logging.info('Datos recibidos para actualizar en la base de datos: ' + str(dict_values))
@@ -151,10 +118,6 @@ def build_json_influx(dict_values, connection_name):
     # Filtrar campos con valor None
     dict_values = {key: value for key, value in dict_values.items() if value is not None}
 
-    # Eliminar el campo 'time' si existe
-    if 'time' in dict_values:
-        del dict_values['time']
-
     # Crear el cuerpo JSON para InfluxDB
     json_body = [
         {
@@ -164,8 +127,9 @@ def build_json_influx(dict_values, connection_name):
     ]
 
     logging.info('Datos que se enviarán a InfluxDB: ' + str(json_body))
-    influx_client.write_points(json_body)
 
+    # Escribir datos en InfluxDB
+    influx_client.write_points(json_body)
 
 def get_previous_values(connection_name):
     # Consulta para obtener el último punto de datos de la serie
@@ -179,38 +143,39 @@ def get_previous_values(connection_name):
             return points[0]  # El último punto de datos
     return {}  # Si no hay puntos previos, devolvemos un diccionario vacío
 
-
 def extract_data(topic, payload):
-    ###Function to extract values from every tag when a data message is received from Databus
     dict_values = dict()
-    ###First we need to identify the connection name
     connection_name = extract_connection_name(topic)
-    #Message comes with an id,value for every tag, we need to know which id refers to which tag name
-    for key,value in payload.items():
-        if (key == 'vals'):
+    if connection_name is None:
+        logging.warning('Connection name could not be extracted from the topic.')
+        return
+    
+    for key, value in payload.items():
+        if key == 'vals':
             vals = value
-            exit
+            break
+    else:
+        logging.warning('No values found in payload.')
+        return
+
     for tag in vals:
+        tag_name = None
         for key, value in tag.items():
-            if (key== 'id'):
+            if key == 'id':
                 tag_id = value
                 tag_name = extract_names_from_metadata(tag_id, connection_name)
-            if (key == 'val'):
-                tag_val= value
+            elif key == 'val' and tag_name:
+                tag_val = value
                 dict_values[tag_name] = tag_val
-    
-    logging.info('Data are sending to database is:'+ str(type(dict_values)))       
-    build_json_influx(dict_values, connection_name)
-    
-    
 
-# The callback for when the client receives a CONNACK response from the server.
+    logging.info('Data are sending to database is:' + str(dict_values))       
+    build_json_influx(dict_values, connection_name)
+
 def on_connect(client, userdata, flags, rc):
-    logging.info("Connected with result code "+ str(rc))
+    logging.info("Connected with result code " + str(rc))
     client.subscribe(MQTT_TOPIC_DATA)
     client.subscribe(MQTT_TOPIC_METADATA)
 
-# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     payload = json.loads(msg.payload.decode('utf-8'))
     topic = msg.topic
@@ -221,26 +186,24 @@ def on_message(client, userdata, msg):
     else:
         logging.info('Unknown topic...')
 
-
+# Llama a load_metadata antes de conectar el cliente MQTT
+load_metadata()
 
 time.sleep(5)
 client = mqtt.Client()
 
 client.on_connect = on_connect
 client.on_message = on_message
-client.username_pw_set(MQTT_USER,MQTT_PASSWORD)
+client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
 client.connect(MQTT_BROKER_SERVER, 1883, 60)
 
 logging.info('Creating influxdb')
 
-influx_client = InfluxDBClient(INFLUXDB_IP,8086,"root","root",INFLUXDB_DATABASE)
+influx_client = InfluxDBClient(INFLUXDB_IP, 8086, "root", "root", INFLUXDB_DATABASE)
 
-#create a new database and use this database
+# Crear una nueva base de datos y usarla
 influx_client.create_database(INFLUXDB_DATABASE)
 influx_client.switch_database(INFLUXDB_DATABASE)
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
+# Llamada bloqueante que procesa el tráfico de red, despacha callbacks y maneja reconexiones.
 client.loop_forever()
