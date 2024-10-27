00f58f436f97835f8e0bd973f63bf4dd5b5cfc0b
Joel <joel-joaquim.rojas@seat.es>
2024-10-27 12:34:21 +0100

    Elimino archivos grandes y genero gitignore

--- data_analysis/data/script_peticion_data.sh
+++
@@ -1,5 +0,0 @@
-curl --request POST "http://172.20.238.9:8086/api/v2/query?org=SEAT%20S.A." ^
-  --header "Authorization: Token my-secret-token" ^
-  --header "Accept: application/csv" ^
-  --header "Content-type: application/vnd.flux" ^
-  --data "from(bucket:\"my-bucket\") |> range(start: -24h) |> filter(fn: (r) => r._measurement == \"KEU2A21-----BS1---KFU1\")" > output.csv 
